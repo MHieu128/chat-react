6ec834696ed661c2c8693b8a0e9e4089593d8f4a
Horace <49237174+MHieu128@users.noreply.github.com>
2024-03-22 01:09:40 +0700

    update UI login

---
+++ src/authPages/LoginPage/LoginPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import AuthBox from "../../shared/components/AuthBox";
+
+const LoginPage = () => {
+  return <AuthBox>Login Page</AuthBox>;
+};
+
+export default LoginPage;
