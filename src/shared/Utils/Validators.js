4596c98e3bf883ee3aab7867419b6df9dc3e27ab
Horace <49237174+MHieu128@users.noreply.github.com>
2024-04-19 00:01:43 +0700

    Update UI

--- src/shared/Utils/Validators.js
+++ src/shared/Utils/Validators.js
@@ -1,7 +1,18 @@
 export const validateLoginForm = ({ username, password }) => {
-  const isUsername = validateUsername(username);
-  const isPasswordValid = validatePassword(password);
-  return isUsername && isPasswordValid;
+  return validateUsername(username) && validatePassword(password);
+};
+
+export const validateRegisterForm = ({ mail, password, username }) => {
+  return (
+    validateMail(mail) &&
+    validateUsername(username) &&
+    validatePassword(password)
+  );
+};
+
+const validateMail = (mail) => {
+  var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  return emailPattern.test(mail);
 };
 
 const validateUsername = (username) => {
